241ca17e415a71f6f2c074b39b71881c84f04753
Todd Pangilinan <pang.todd@gmail.com>
2022-05-05 14:11:34 -0700

    establish test for #save.

--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -3,7 +3,17 @@ require 'word'
 
 describe '#Word' do
   describe('.all')do
-    it("returns an empty array when there are no words")
+    it("returns an empty array when there are no words")do
       expect(Word.all).to eq([])
+    end
+  end
+  describ('#save')do
+    it("saves a word")do
+      word = Word.new("frog", nil)
+      word.save()
+      word2 = Word.new("dog", nil)
+      word2.save()
+      expect(Word.all).to eq([word, word2])
+    end
   end
 end
