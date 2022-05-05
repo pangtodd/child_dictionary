c668ad1104554f8607eaa50c8ae2256571422a34
Todd Pangilinan <pang.todd@gmail.com>
2022-05-05 15:11:35 -0700

    establish words.erb view and updated app.rb routes for / and /words.

--- app.rb
+++ app.rb
@@ -1,34 +1,40 @@
 require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('pry')
+also_reload('lib/**/*.rb')
 
 get('/') do
-  "This will be our home page. '/' is always the root route in a Sinatra application."
+  @words = Word.all
+  erb(:words)
 end
 
-get('/albums') do
-  "This route will show a list of all albums."
+get('/words') do
+  @words = Word.all
+  erb(:words)
 end
 
-get('/albums/new') do
+get('/words/new') do
   "This will take us to a page with a form for adding a new album."
 end
 
-get('/albums/:id') do
+get('/words/:id') do
   "This route will show a specific album based on its ID. The value of ID here is #{params[:id]}."
 end
 
-post('/albums') do
+post('/words') do
   "This route will add an album to our list of albums. We can't access this by typing in the URL. In a future lesson, we will use a form that specifies a POST action to reach this route."
 end
 
-get('/albums/:id/edit') do
+get('/words/:id/edit') do
   "This will take us to a page with a form for updating an album with an ID of #{params[:id]}."
 end
 
-patch('/albums/:id') do
+patch('/words/:id') do
   "This route will update an album. We can't reach it with a URL. In a future lesson, we will use a form that specifies a PATCH action to reach this route."
 end
 
-delete('/albums/:id') do
+delete('/words/:id') do
   "This route will delete an album. We can't reach it with a URL. In a future lesson, we will use a delete button that specifies a DELETE action to reach this route."
 end
 
