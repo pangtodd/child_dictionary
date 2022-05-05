fcbfec89cd02525364b5b5777f7934df82e67d92
Todd Pangilinan <pang.todd@gmail.com>
2022-05-05 14:38:56 -0700

    establish #== and #save methods, both pass tests.

--- lib/word.rb
+++ lib/word.rb
@@ -1,11 +1,23 @@
 class Word
+  attr_reader :id, :name
+  
   @@words = {}
+  @@total_rows = 0
 
-  def initialize(name)
+  def initialize(name, id)
     @name = name
+    @id = id || @@total_rows += 1
   end
 
   def self.all
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new(self.name, self.id)
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
   end
 end
