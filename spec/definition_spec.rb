bdd02ba5d02813594d51957dfe18213b5222b4d3
Todd Pangilinan <pang.todd@gmail.com>
2022-05-06 13:22:08 -0700

    establish test for find_by_word method.

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -13,22 +13,26 @@ describe '#Definition' do
   end
 
   describe('#==')do
-    it("is the same definition if it has the attributes as another definition")
-      definition = Defintion.new("aquatic animal", @word.id, nil)
+    it("is the same definition if it has the attributes as another definition")do
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition2 = Definition.new("aquatic animal", @word.id, nil)
       expect(definition).to eq(definition2)
     end
   end
   describe('.all')do
     it("returns a list of all definitions")do
-      definition 
+      definition = Definition.new("aquatic animal", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("large fish", @word.id, nil)
+      definition2.save()
+      expect(Definition.all).to eq([definition, definition2])
     end
   end
   describe('.clear')do
     it("clears all definitions")do
-      definition = Defintion.new("aquatic animal", @word.id, nil)
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition.save()
-      defintion2 = Definition.new("flying animal", @word.id, nil)
+      definition2 = Definition.new("large fish", @word.id, nil)
       definition2.save
       Definition.clear
       expect(Definition.all).to eq([])
@@ -36,36 +40,45 @@ describe '#Definition' do
   end
   describe('#save')do
     it("saves a definition")do
-      definition = Defintion.new("aquatic animal", @word.id, nil)
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition.save()
-      expect(Defintiton.all).to eq([song])
+      expect(Definition.all).to eq([definition])
     end
   end
   describe('.find')do
     it("find a definition by id")do
-      definition = Defintion.new("aquatic animal", @word.id, nil)
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition.save()
-      defintion2 = Definition.new("flying animal", @word.id, nil)
+      definition2 = Definition.new("large fish", @word.id, nil)
       definition2.save()
       expect(Definition.find(definition.id)).to eq(definition)
     end
   end
   describe('#update')do
     it("updates a definition by id")do
-      defintion = Definition.new("flying animal", @word.id, nil)
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition.save()
-      definition.update("flying animal with feathers", @word.id)
-      expect(definition.name).to eq("flying animal with feathers")
+      definition.update("aquatic animal with big teeth", @word.id)
+      expect(definition.name).to eq("aquatic animal with big teeth")
     end
   end
   describe('#delete')do
     it("deletes a definition by id")do
-      definition = Defintion.new("aquatic animal", @word.id, nil)
+      definition = Definition.new("aquatic animal", @word.id, nil)
       definition.save()
-      defintion2 = Definition.new("flying animal", @word.id, nil)
+      definition2 = Definition.new("big fish", @word.id, nil)
       definition2.save()
       definition.delete
       expect(Definition.all).to eq([definition2])
+    end
+  end
+  describe('.find_by_word') do
+    it("finds definitions for an word") do
+      definition = Definition.new("aquatic animal", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("big fish", @word.id, nil)
+      definition2.save()
+      expect(Definition.find_by_word(definition2.id)).to(eq([definition2]))
     end
   end
 end
